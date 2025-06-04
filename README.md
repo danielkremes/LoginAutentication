3b4139609840f01df88d12b2bdef1b754598bd32
DanielKremes <danielkremes06@gmail.com>
2025-06-04 18:08:20 -0300

    chore: Project Created

---
+++ README.md
@@ -0,0 +1,12 @@
+# Spring Security
+
+# Description
+	- Autenticatin and acess permissions in API with spring security and JWT
+
+# Prerequisites
+	- REQUIRED: Java 17 or superior
+    - REQUIRED: dependency(Spring Security)
+	- OPTIONAL: dependency(lombok)
+
+# Last Updated
+    - 6/4/2025
---
+++ src/main/java/com/drk/spring_security/spring_security/security/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.drk.spring_security.spring_security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService () {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USERS")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated()
+        ).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
