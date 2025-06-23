36a0aafffa59147801c52d2e229b0a38f1efafbe
DanielKremes <danielkremes06@gmail.com>
2025-06-23 17:44:45 -0300

    feat: add docker composer dependency

--- src/main/java/com/drk/spring_security/spring_security/controllers/UserController.java
+++ src/main/java/com/drk/spring_security/spring_security/controllers/UserController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/login")
 public class UserController {
 
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home() {
         return "Welcome Login Authentication";
     }
